a4815626aa8e489ffa11d43e918668d5aeb902ae
Hiroharu Tanaka <hiroharu8864@gol.com>
2022-07-25 07:54:28 +0900

    動作確認完了

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -2,7 +2,6 @@ import { FC, memo, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import { FormContainer } from "../molecules/FormContainer";
 import { MainButton } from "../atoms/MainButton";
-import { Input } from "../atoms/Input";
 
 export const Home: FC = memo(() => {
   const navigate = useNavigate();
@@ -14,7 +13,6 @@ export const Home: FC = memo(() => {
     <>
       <FormContainer>
         <h3>GraphQL API</h3>
-        <Input placeholder="リポジトリ対象ユーザ入力" />
         <MainButton onClick={onClickFetchCsv}>GraphQL API</MainButton>
       </FormContainer>
     </>
